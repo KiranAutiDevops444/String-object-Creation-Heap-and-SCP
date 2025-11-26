95f6cfcaa02afb936204a25a746627923a73790f
KiranAutiDevops444 <kiranauti2003@gmail.com>
2025-11-26 23:07:59 +0530

    Add files via upload

---
+++ AsciiSubtraction.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class AsciiSubtraction{
+   public static char Capital(char ch){
+    int ascii=(int)ch;
+    int add=ascii-32;
+    
+    char ch1=(char)add;
+    return ch1;
+   }
+
+   public static void main(String[] args){
+    Scanner sc=new Scanner(System.in);
+    System.out.println("Enter Character:");
+    char ch=sc.next().charAt(0);
+    System.out.println(Capital(ch));
+   }
+}
