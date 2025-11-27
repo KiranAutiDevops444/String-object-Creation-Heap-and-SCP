5a4b5db1e8a6e3b8e1591729173fb78bcd8a9c39
KiranAutiDevops444 <kiranauti2003@gmail.com>
2025-11-27 23:21:46 +0530

    Add files via upload

---
+++ StrTrim.java
@@ -0,0 +1,10 @@
+public class StrTrim{
+    public static String trim(String s){
+        String s1=s.trim();
+        return s1;
+    }
+    public static void main(String[] args) {
+        String s="   Kiran Laxman Auti   ";
+        System.out.println(trim(s));
+    }
+}
